1683b6bf6598c8c9fdbe616ca7b4a184136aab09
Shubham Kumar Raj <131697566+shubham2845@users.noreply.github.com>
2023-08-28 22:24:24 +0530

    Create front_page.php

---
+++ front_page.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="styleforlink.css">
+</head>
+<body>
+
+<img src="logoformain.png" alt="Logo" class="logo">
+   <div class="first">
+       
+        <a href="front.php">HOME</a>
+        <a href="display.php">TO DO LIST</a>
+        <a href="form.php">ADD DETAILS</a>
+   </div>
+
+
+   <div class="middle">
+        <a href="form.php" class="hero-btn"> Get Started </a>
+   </div>
+
+
+
+
+   <div class="second">
+        
+        <h2> To Do List! </h2>
+        <p>A "to-do list" is a simple and effective organizational tool used to manage tasks, chores, or activities that need to be completed within a certain period of time. It serves as a way to keep track of what needs to be done and helps individuals stay organized and focused on their goals. Here are some key aspects and benefits of using a to-do list:</p>
+
+   </div>
+
+    <div class="third">
+        <p>1. Task Management: A to-do list provides a centralized place to jot down tasks, making it easier to manage and remember what needs to be accomplished.</br>
+
+        2. Prioritization: By listing tasks in order of importance or urgency, you can prioritize your work and focus on what matters most.</br>
+
+        3. Time Management: To-do lists can help you allocate time for specific tasks and prevent you from getting overwhelmed by tasks that need immediate attention.</br>
+
+        4. Productivity: Crossing off completed tasks gives a sense of accomplishment and motivation, boosting productivity.</br>
+
+        5. Reduced Stress: When you have a clear plan for what needs to be done, you can avoid last-minute rushes and reduce stress related to forgetting important tasks.</p>
+
+    </div>
+
+
+</body>
+</html>
